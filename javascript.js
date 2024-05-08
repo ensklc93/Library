319c3fea554ad4a1b8ff2a73b30846dbc130f9c8
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-05-08 08:35:59 +0200

    Added button and changed input required
    Adding the button separate the functions of adding book and showing library

--- javascript.js
+++ javascript.js
@@ -3,9 +3,15 @@ const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
 const addToLibrary = document.querySelector("#addToLibrary");
-const divDeneme =  document.querySelector(".deneme");
+const showLibrary = document.querySelector("#showLibrary");
 
-const myLibrary = [];
+const divDeneme = document.querySelector(".deneme");
+
+const myLibrary = [
+  new Book("Lotr", "J.R.R. Tolkien", 567, "not read"),
+  new Book("Dice", "Adam Flawes", 546, "read"),
+  new Book("Dice", "Adam Flawes", 546, "read"),
+];
 
 function Book(title, author, pageNumbers, read) {
   (this.title = title),
@@ -22,11 +28,49 @@ function addBookToLibrary() {
     titleInput.value,
     authorInput.value,
     pagesInput.value,
-    readInput.value
+    readInput.value,
+    
   );
   return myLibrary.push(newBook);
 }
 
-addToLibrary.addEventListener("click", () => {
+addToLibrary.addEventListener("click", (e) => {
   addBookToLibrary();
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
+  e.preventDefault();
 });
+
+showLibrary.addEventListener("click", () => {
+  showList();
+});
+
+
+
+function showList() {
+  for (let key in myLibrary) {
+    let divCard = document.createElement("div");
+    divDeneme.appendChild(divCard);
+    let titleH = document.createElement("h1");
+    titleH.textContent = myLibrary[key].title;
+    divCard.appendChild(titleH);
+    let authortext = document.createElement("p");
+    authortext.textContent = myLibrary[key].author;
+    divCard.appendChild(authortext);
+    let pageNumbersText = document.createElement("p");
+    pageNumbersText.textContent = myLibrary[key].pageNumbers;
+    divCard.appendChild(pageNumbersText);
+    let readText = document.createElement("p");
+    readText.textContent = myLibrary[key].read;
+    divCard.appendChild(readText);
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    divCard.appendChild(deleteButton);
+  }
+}
+
+
+
+ 
