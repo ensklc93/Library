1b21503550805441883a4941c8e9d2add291a62d
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-05-21 20:36:38 +0200

    Added delete button, show/hide button, to toggle
    Added click events for the buttons
    
    Set data-id for newly created divs

--- javascript.js
+++ javascript.js
@@ -2,15 +2,12 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
+const notreadInput = document.querySelector("#not-read");
 const addToLibrary = document.querySelector("#addToLibrary");
 const showLibrary = document.querySelector("#showLibrary");
-const container = document.querySelector(".container");
+const container = document.querySelector(".container-show");
 
-const myLibrary = [
-  new Book("Lotr", "J.R.R. Tolkien", 567, "not read"),
-  new Book("Dice", "Adam Flawes", 546, "read"),
-  new Book("Dice", "Adam Flawes", 546, "read"),
-];
+let myLibrary = [];
 
 function Book(title, author, pageNumbers, read) {
   (this.title = title),
@@ -20,6 +17,7 @@ function Book(title, author, pageNumbers, read) {
     (this.info = function () {
       return `${this.title} is written by ${this.author} has total pages of ${this.pageNumbers} and I did ${this.read} it`;
     });
+  this.id = title + pageNumbers;
 }
 
 function addBookToLibrary() {
@@ -33,48 +31,73 @@ function addBookToLibrary() {
 }
 
 addToLibrary.addEventListener("click", (e) => {
-  deneme();
-  listOfBooks();
+  if (
+    titleInput.value === "" ||
+    authorInput.value === "" ||
+    pagesInput.value === "" ||
+    read.value === ""
+  ) {
+    return;
+  }
   addBookToLibrary();
+  removeList();
   titleInput.value = "";
   authorInput.value = "";
   pagesInput.value = "";
   readInput.value = "";
+  listOfBooks();
   e.preventDefault();
 });
 
 function listOfBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
     const usersBox = document.createElement("div");
-  
+    const deleteButton = document.createElement("button");
     const authorText = document.createElement("p");
     const headerText = document.createElement("h1");
     const pageNumbersText = document.createElement("p");
     const readText = document.createElement("p");
     const infoText = document.createElement("p");
+    const readButton = document.createElement("button")
 
     authorText.innerText = `${myLibrary[i].author}`;
     headerText.innerText = `${myLibrary[i].title}`;
     pageNumbersText.innerText = `${myLibrary[i].pageNumbers}`;
     readText.innerText = `${myLibrary[i].read}`;
     infoText.innerText = `${myLibrary[i].info()}`;
+    deleteButton.innerText = "Delete";
+    deleteButton.dataset.id = `${myLibrary[i].id}`;
+    readButton.innerText = "Read/Not Read"
 
     usersBox.appendChild(headerText);
     usersBox.appendChild(authorText);
     usersBox.appendChild(pageNumbersText);
     usersBox.appendChild(readText);
+    usersBox.appendChild(readButton);
     usersBox.appendChild(infoText);
+    usersBox.appendChild(deleteButton);
 
     container.appendChild(usersBox);
-  }
 
-  showLibrary.addEventListener("click", () => {
-   container.classList.toggle('container-show')
-  });
+
+    deleteButton.addEventListener("click", () => {
+      let ArrayId = deleteButton.getAttribute("data-id");
+      if (ArrayId === myLibrary[i].id) {
+        container.removeChild(usersBox)
+        myLibrary = myLibrary.filter(users => users.id !== myLibrary[i].id)
+      }
+    });
+  }
 }
 
-function deneme() {
+showLibrary.addEventListener("click", () => {
+  container.classList.toggle("container-show");
+});
+
+function removeList() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
+
+listOfBooks();
